3f9dfdb9e102f0b660f494f9e8173c3326278aad
HUgo Ocampo <hdocampo@gmail.com>
2020-09-03 18:52:34 -0300

    changed domain

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   name: 'fisioespalda',
   state: {
     frontity: {
-      url: 'https://fisioespalda.neoimage.net',
+      url: 'https://fisioespalda.com.uy',
       title: 'Clínica Fisioespalda especialistas tratamientos de espalda y columna, dolor crónico y fibromialgia',
       description: 'En clínica Fisioespalda encontrarás tratamiento especializado para tu dolor de columna, dolor de espalda, fibromialgia y mucho más, a cargo de un equipo altamente calificado y con atención humana.',
       keywords: 'Dolor de espalda, Fisioterapia, Fibromialgia, Infrared, Dolor de Columna, Hidroterapia, Ciática, Ciatalgia, Dorsalgia, Dolor Cervical, Artrosis de Columna vertebral, Hernias y Protrusiones de disco, Canal cervical,  Lumbar estrecho, Cervicalgia-cervicobraquialgia, Dorsalgia, Lumbalgia, Lumbociatalgia, Lumbocruralgia, Lumbalgia en el embarazo, Escoliosis, Espondilolistesis, Espondilólisis, Postoperatorios de cirugía de columna (Artrodesis, cifoplastia, etc.), Dolor crónico miofascial, Frontity'

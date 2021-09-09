64a364b8c5dc384ef460b2236809e00e724ee4ac
Hugo Ocampo <hdocampo@gmail.com>
2021-09-09 00:28:43 -0300

    Added payment method

--- packages/mars-theme/src/components/slider/styles.js
+++ packages/mars-theme/src/components/slider/styles.js
@@ -15,8 +15,6 @@ export default makeStyles(theme => ({
     }
   },
   slider: {
-    background: `url(${require("../../assets/fisioespalda-slide-home-1.jpg")})`,
-    backgroundSize: 'cover',
     color: WHITE,
     width: '100%',
     height: '100%',
@@ -38,7 +36,12 @@ export default makeStyles(theme => ({
     }
   },
   sliderButton: {
-    ...BUTTON_STYLE_LIGHT,
+    '&, & a': {
+      ...BUTTON_STYLE_LIGHT,
+    },
+    '& a': {
+      background: 'none',
+    }
   },
   sliderText: {
     maxWidth: '40%',
